b8cd9cd33209272b5bada71a9acf7b69d0435824
Hyddenzone <alexbiersack00@gmail.com>
2025-11-03 16:45:57 -0300

    Update index.php

--- php/db/index.php
+++ php/db/index.php
@@ -1,10 +1,8 @@
 <?php
-
 $pdo = new PDO('sqlite:db/database.sqlite');
 $stmt = $pdo->query("SELECT title, url FROM links");
 $links = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -14,19 +12,13 @@ $links = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </head>
 <body>
     <div class="container">
-        <h1>Meus Links Importantes</h1>
+        <h1>Meus Links</h1>
         <ul>
-            <?php foreach ($links as $link): ?>
-                <li>
-                    <a href="<?php echo htmlspecialchars($link['url']); ?>" target="_blank">
-                        <?php echo htmlspecialchars($link['title']); ?>
-                    </a>
-                </li>
+            <?php foreach ($links as $l): ?>
+                <li><a href="<?php echo $l['url']; ?>" target="_blank"><?php echo $l['title']; ?></a></li>
             <?php endforeach; ?>
         </ul>
-        <a href="login.php" class="admin-link">Admin</a>
+        <a href="login.php">Admin</a>
     </div>
 </body>
 </html>
-
-
