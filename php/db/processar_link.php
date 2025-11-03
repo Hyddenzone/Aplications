56ffdc2aaa41b426c89247d3a7ff589934e3b75f
Hyddenzone <alexbiersack00@gmail.com>
2025-11-03 16:46:34 -0300

    Update processar_link.php

--- php/db/processar_link.php
+++ php/db/processar_link.php
@@ -8,25 +8,20 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 
 $pdo = new PDO('sqlite:db/database.sqlite');
 
-
 if ($_POST['action'] == 'add') {
     $title = $_POST['title'];
     $url = $_POST['url'];
-    
-   
-    if (!empty($title) && !empty($url) && filter_var($url, FILTER_VALIDATE_URL)) {
+    if ($title && $url && filter_var($url, FILTER_VALIDATE_URL)) {
         $stmt = $pdo->prepare("INSERT INTO links (title, url) VALUES (?, ?)");
         $stmt->execute([$title, $url]);
     }
 }
-
 
 if ($_POST['action'] == 'delete') {
     $id = $_POST['id'];
     $stmt = $pdo->prepare("DELETE FROM links WHERE id = ?");
     $stmt->execute([$id]);
 }
-
 
 header("Location: admin.php");
 exit;
