a68d290d98fec7e917b7bcc9adfe63bc9b12feb8
Hyddenzone <alexbiersack00@gmail.com>
2025-11-03 16:47:50 -0300

    Update setup.php

--- php/db/setup.php
+++ php/db/setup.php
@@ -1,28 +1,23 @@
 <?php
-
 try {
     $pdo = new PDO('sqlite:db/database.sqlite');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    
     $pdo->exec("CREATE TABLE IF NOT EXISTS links (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         url TEXT NOT NULL
     )");
 
-    
     $pdo->exec("CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT NOT NULL,
         password TEXT NOT NULL
     )");
 
-
     $pdo->exec("INSERT INTO users (username, password) VALUES ('admin', 'admin')");
 
-    echo "Banco de dados e tabelas criados com sucesso!";
-
+    echo "Banco criado com sucesso!";
 } catch (PDOException $e) {
-    echo "Falha na conexão: " . $e->getMessage();
+    echo "Erro: " . $e->getMessage();
 }
